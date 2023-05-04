389fbd9714316d319127a7fa992fd4630d01713a
SerhiiKutovyi <wagenrex@gmail.com>
2023-05-04 15:10:19 +0300

    Update App.js

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Hello RegistrationScreen !!!</Text>
+      <Text style={styles.text}>Hello RegistrationScreen!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,5 +18,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'green',
+    fontSize: 20,
   },
 });
