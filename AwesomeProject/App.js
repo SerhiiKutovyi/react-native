80735f0a3c2421349b788f22a51331317f7036bd
SerhiiKutovyi <wagenrex@gmail.com>
2023-05-04 14:35:32 +0300

    test

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,16 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-import { StyleSheet, Text, View } from "react-native";
-import RegistrationScreen from "./src/components/RegistrationScreen/RegistrationScreen";
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello RegistrationScreen !!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
 
-export default function App() {
-  return <View style={styles.container}>{RegistrationScreen}</View>;
-}
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
---
+++ AwesomeProject/src/components/Screens/RegistrationScreen/RegistrationScreen.jsx
@@ -0,0 +1,22 @@
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, Text, View } from "react-native";
+
+// const RegistrationScreen = () => {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hello RegistrationScreen !!!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// };
+
+// export default RegistrationScreen;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
