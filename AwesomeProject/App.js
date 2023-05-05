8063662c36127d36c0f9df6dff3ea5c7316279af
SerhiiKutovyi <wagenrex@gmail.com>
2023-05-05 18:53:39 +0300

    Icon plus

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -32,7 +32,7 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     justifyContent: 'flex-end',
-    // textAlign: 'center',
+    textAlign: 'center',
     // alignItems: 'center',
   },
 });
